d62765cbc4888485f1874e6ee93e2201f7b1beb6
vedaantBali <vedaantbali@gmail.com>
2021-06-24 11:12:14 +0530

    finalizations

--- index.js
+++ index.js
@@ -156,7 +156,7 @@ if(process.env.GENERATE_PEER_PORT === 'true') {
     PEER_PORT = DEFAULT_PORT + Math.ceil(Math.random() * 1000);
 }
 
-const PORT = process.env.PORT || PEER_PORT || DEFAULT_PORT;
+const PORT = PEER_PORT || DEFAULT_PORT;
 
 app.listen(PORT, () => {
     console.log(`Application is listening at localhost:${PORT}`);
