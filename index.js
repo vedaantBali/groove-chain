67d86a5c8f4f3d03ab8ba6ba9321b700b6b4d747
vedaantBali <vedaantbali@gmail.com>
2021-08-24 10:50:31 +0530

    checking functionality

--- index.js
+++ index.js
@@ -7,13 +7,10 @@ const TransactionPool = require('./wallet/transaction-pool');
 const Wallet = require('./wallet');
 const TransactionMiner = require('./app/transaction-miner');
 
-const REDIS_SERVER = 'redis://:p9d882fe1f2014a682580409ffa09e0dd4ac69a78a138ccf5b839feb1537d62bd@ec2-34-206-7-149.compute-1.amazonaws.com:21029';
-const REDIS_LOCAL = 'redis://127.0.0.1:6379';
-
 const isDevelopment = process.env.ENV === 'development';
 const REDIS_URL = isDevelopment ?
-    REDIS_LOCAL :
-    REDIS_SERVER;
+'redis://127.0.0.1:6379' :
+    'redis://:p9d882fe1f2014a682580409ffa09e0dd4ac69a78a138ccf5b839feb1537d62bd@ec2-34-206-7-149.compute-1.amazonaws.com:21029';
 const DEFAULT_PORT = 3000;
 const ROOT_NODE_ADDRESS = `http://localhost:${DEFAULT_PORT}`;
 
