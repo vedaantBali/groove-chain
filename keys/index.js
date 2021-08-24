5d5f10a5aabd023513f77108dda7bf1590e1c0d5
vedaantBali <vedaantbali@gmail.com>
2021-08-24 10:22:26 +0530

    redis updates

--- keys/index.js
+++ keys/index.js
@@ -1,3 +1,3 @@
 const REDIS_SERVER = 'redis://:p9d882fe1f2014a682580409ffa09e0dd4ac69a78a138ccf5b839feb1537d62bd@ec2-34-206-7-149.compute-1.amazonaws.com:21029';
 const REDIS_LOCAL = 'redis://127.0.0.1:6379';
-module.exports = REDIS_URL, REDIS_LOCAL;
+module.exports = REDIS_SERVER, REDIS_LOCAL;
