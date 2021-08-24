a732e41d993c52d8d0beb5190c2d3b22c7322d25
vedaantBali <vedaantbali@gmail.com>
2021-08-24 10:13:36 +0530

    minor changes

--- keys/index.js
+++
@@ -1,3 +0,0 @@
-const REDIS_URL = 'redis://:p9d882fe1f2014a682580409ffa09e0dd4ac69a78a138ccf5b839feb1537d62bd@ec2-34-206-7-149.compute-1.amazonaws.com:21029';
-const REDIS_LOCAL = 'redis://127.0.0.1:6379';
-module.exports = REDIS_URL, REDIS_LOCAL;
